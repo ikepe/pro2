9f66eb896ece428b1dba5d4146aa44f3a2cb82fb
ikepe <kagokakidaiishidai@gmail.com>
2021-04-16 15:31:36 +0900

    Create 3-7.c

---
+++ 3-7.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int x1 , x2 , x3 , min ;
+
+    printf("x1?: ");
+    scanf("%d" , &x1);
+    printf("x2?: ");
+    scanf("%d" , &x2);
+    printf("x3?: ");
+    scanf("%d" , &x3);
+
+    min = x1;
+    if(min > x2) min = x2;
+    if(min > x3) min = x3;
+
+    printf("min = %d" , min);
+
+    return 0;
+}
